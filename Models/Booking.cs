83ae1ff046562cfc6030ee31f84551d3da3d6301
ItsMeOngitHub <51195218+GitHubuser-a@users.noreply.github.com>
2022-10-30 22:49:36 -0400

    Add files via upload

---
+++ Models/Booking.cs
@@ -0,0 +1,27 @@
+﻿using HotelBooking.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBooking.Models
+{
+    public class Booking
+    {
+        public Reservation Reservation { get; set; }
+        public Room Room { get; set; }
+        public Customer Customer { get; set; }
+
+        public Booking()
+        {
+            Reservation = new Reservation();
+            Room = new Room();
+            Customer = new Customer();
+        }
+
+
+        
+    }
+}
